d5f93566af99d6e29951299a045a60b0aec2abc0
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-16 19:55:08 -0300

    feat: App Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
+      <BrowserRouter >
+        <Header />
+        <Routes>
+          <Route path="/" element={ <Home /> }/>
+          <Route path="/login" element={ <Login /> }/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
