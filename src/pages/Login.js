20fdfffdb05f3d5321921862aa204147e40c0917
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-19 22:59:56 -0300

    feat: loginPage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from '../components/LoginForm/LoginForm';
+import LoginCreate from '../components/LoginCreate/LoginCreate';
+import LoginRecover from '../components/LoginRecover/LoginRecover';
+import LoginReset from '../components/LoginReset/LoginReset';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Routes >
+        <Route path='/' element={<LoginForm />}/>
+        <Route path='create' element={<LoginCreate />}/>
+        <Route path='recover' element={<LoginRecover />}/>
+        <Route path='reset' element={<LoginReset />}/>
+      </Routes>
+    </div>
   )
 }
 
