40886d4c112dac6e47d60aaf8dcbf8097b882417
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-19 22:54:31 -0300

    feat: constants endpoints

---
+++ src/constants/Endpoints.js
@@ -0,0 +1,27 @@
+const USER_POST = '/api/user';
+const TOKEN_POST = '/jwt-auth/v1/token';
+const TOKEN_VALIDATE_POST = '/jwt-auth/v1/token/validate';
+const USER_GET = '/api/user';
+const PHOTO_POST = '/api/photo';
+const PHOTO_GET = '/api/photo';
+const PHOTO_DELETE = '/api/photo/:id';
+const COMMENT_POST = '/api/comment/:id';
+const COMMENT_GET = '/api/comment/:id';
+const STATS_GET = '/api/stats';
+const PASSWORD_LOST = '/api/password/lost';
+const PASSWORD_RESET_POST = '/api/password/reset';
+
+export {
+  USER_GET,
+  USER_POST,
+  TOKEN_POST,
+  TOKEN_VALIDATE_POST,
+  PHOTO_DELETE,
+  PHOTO_GET,
+  PHOTO_POST,
+  COMMENT_POST,
+  COMMENT_GET,
+  STATS_GET,
+  PASSWORD_LOST,
+  PASSWORD_RESET_POST,
+};
