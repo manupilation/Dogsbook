efd3e4ecce3580e3330a101a8f200fc169b0f86f
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-19 23:00:21 -0300

    refactor: app UserContext

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import UserContext from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter >
-        <Header />
-        <Routes>
-          <Route path="/" element={ <Home /> }/>
-          <Route path="/login" element={ <Login /> }/>
-        </Routes>
-        <Footer />
+        <UserContext>
+          <Header />
+          <Routes>
+            <Route path="/" element={ <Home /> }/>
+            <Route path="/login/*" element={ <Login /> }/>
+          </Routes>
+          <Footer />
+        </UserContext>
       </BrowserRouter>
     </div>
   );
