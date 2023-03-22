3c58bbd8333ed3ca85e98c80ffc2d8e69203e425
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-22 16:46:30 -0300

    feat: App using protectedRoute to user route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import ProtectedRoute from './components/Helper/ProtectedRoute';
+import User from './components/User/User';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import UserContext from './UserContext';
@@ -16,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={ <Home /> }/>
             <Route path="/login/*" element={ <Login /> }/>
+            <Route path="/conta/*" element={ <ProtectedRoute><User /></ProtectedRoute> }/>
           </Routes>
           <Footer />
         </UserContext>
