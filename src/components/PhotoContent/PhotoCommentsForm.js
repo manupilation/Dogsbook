b344f88bcf2423d2cb39fb5b142303ed05bb85e0
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-23 12:54:28 -0300

    feat: PhotoCommentsForm

---
+++ src/components/PhotoContent/PhotoCommentsForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { COMMENT_POST_FETCH } from '../../api';
+import {ReactComponent as Enviar} from "../../Assets/enviar.svg";
+import useFetch from '../../hooks/useFetch';
+import Error from '../Helper/Error';
+import styles from "./PhotoCommentsForm.module.css";
+
+const PhotoCommentsForm = ({id, setComments}) => {
+  const [comment, setComment] = useState("");
+  const {request, error} = useFetch();
+  const token = window.localStorage.getItem('token');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const {url, options} = COMMENT_POST_FETCH(id, {comment}, token);
+    const {response, json} = await request(url, options);
+
+    if (response.ok === true) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <textarea
+        className={styles.textarea}
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({target}) => setComment(target.value)}
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
