2afadc0b6044bc14df2b38d7c0ff1fde3a3d9cb7
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-23 12:43:29 -0300

    feat: api new methods

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import URL from "./constants/URL";
 import {
+  COMMENT_POST as COMMENT_POST_URL,
   PHOTO_POST as PHOTO_POST_URL,
   TOKEN_POST as TOKEN_POST_URL,
   TOKEN_VALIDATE_POST,
@@ -69,3 +70,38 @@ export function PHOTO_POST_FETCH(body, token) {
     }
   }
 }
+
+export function PHOTOS_GET_FETCH({page, total, user}) {
+  return {
+    url: URL + PHOTO_POST_URL + `/?_page=${page}&_total=${total}$_user=${user}`,
+    options: {
+      method: "GET",
+      cache: "no-store",
+    }
+  }
+}
+
+export function PHOTO_GET_FETCH(id) {
+  return {
+    url: URL + PHOTO_POST_URL + `/${id}`,
+    options: {
+      method: "GET",
+      cache: "no-store",
+    }
+  }
+}
+
+export function COMMENT_POST_FETCH(id, body, token) {
+  return {
+    url: URL + COMMENT_POST_URL + `/${id}`,
+    options: {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(body),
+    },
+  }
+}
+
