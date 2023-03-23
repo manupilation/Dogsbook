772aeab9422d77d5a776def225f423179bac3cfa
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-23 12:50:41 -0300

    feat: Feed component

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import FeedPhotos from '../FeedPhotos/FeedPhotos';
+import FeedModal from './FeedModal';
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = useState(null);
+
   return (
-    <div>Feed</div>
-  )
+    <div>
+      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/>}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
