979d7ac535e1d7be7bfeacd95fa54b31e4c4b6f1
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-23 12:54:08 -0300

    feat: PhotoComments

---
+++ src/components/PhotoContent/PhotoComments.js
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { userContext } from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from "./PhotoComments.module.css";
+
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const {login} = useContext(userContext);
+  const commentsSection = useRef(null);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map(comment => (
+        <li key={comment.comment_ID}>
+          <b>{comment.comment_author}</b>
+          <span>{comment.comment_content}</span>
+        </li>))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
+  );
+};
+
+export default PhotoComments;
