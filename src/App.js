36b0ebff111f4b8b8d90c53342c620d7bc579a79
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-24 21:19:54 -0300

    feat: APP with NotFound, Photo and UserProfile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./App.css";
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import ProtectedRoute from './components/Helper/ProtectedRoute';
+import NotFound from './components/NotFound/NotFound';
+import Photo from './components/PhotoContent/Photo';
 import User from './components/User/User';
+import UserProfile from './components/User/UserProfile';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import UserContext from './UserContext';
@@ -19,6 +22,9 @@ function App() {
             <Route path="/" element={ <Home /> }/>
             <Route path="/login/*" element={ <Login /> }/>
             <Route path="/conta/*" element={ <ProtectedRoute><User /></ProtectedRoute> }/>
+            <Route path="/foto/:id" element={ <Photo /> }/>
+            <Route path="/perfil/:user" element={ <UserProfile /> }/>
+            <Route path="*" element={ <NotFound /> }/>
           </Routes>
           <Footer />
         </UserContext>
