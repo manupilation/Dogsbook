bbbe336dd3097ee415054c70947a23abb6631b81
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-24 22:24:55 -0300

    feat: LoginRecover heading

--- src/components/LoginRecover/LoginRecover.js
+++ src/components/LoginRecover/LoginRecover.js
@@ -5,6 +5,7 @@ import useForm from '../../hooks/useForm';
 import Button from '../Form/Button/Button';
 import Input from '../Form/Input/Input';
 import Error from '../Helper/Error';
+import Head from '../Helper/Head';
 
 const LoginRecover = () => {
   const login = useForm();
@@ -14,13 +15,14 @@ const LoginRecover = () => {
     event.preventDefault();
 
     if (login.validate()) {
-      const {url, options} = PASSWORD_LOST_FETCH({login: login.value, url: window.location.href.replace("pedeu", "resetar")});
+      const {url, options} = PASSWORD_LOST_FETCH({login: login.value, url: window.location.href.replace("recover", "reset")});
       await request(url, options);
     }
   }
 
   return (
     <section>
+      <Head title="Perdeu a senha ?" description=""/>
       <h1 className='title'>Perdeu a senha ?</h1>
       {
         data ? (
