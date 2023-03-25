89e2aec329313721a4cfe7fbd068d7bbd376883d
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-24 22:04:19 -0300

    feat: Login recover form

--- src/components/LoginRecover/LoginRecover.js
+++ src/components/LoginRecover/LoginRecover.js
@@ -15,27 +15,32 @@ const LoginRecover = () => {
 
     if (login.validate()) {
       const {url, options} = PASSWORD_LOST_FETCH({login: login.value, url: window.location.href.replace("pedeu", "resetar")});
-      const { json } = await request(url, options);
+      await request(url, options);
     }
-
   }
 
   return (
     <section>
       <h1 className='title'>Perdeu a senha ?</h1>
-      <form onSubmit={handleSubmit}>
-        <Input
-          label="Email/Usuário"
-          type="text"
-          name="email"
-          {...login}
-        />
-        {loading 
-          ? <Button disabled>Enviando...</Button>
-          : <Button >Enviar Email</Button>
-        }
-        <Error error={error}/>
-      </form>
+      {
+        data ? (
+          <p style={{color: "#4C1"}}>{data}</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+          <Input
+            label="Email/Usuário"
+            type="text"
+            name="email"
+            {...login}
+          />
+          {loading 
+            ? <Button disabled>Enviando...</Button>
+            : <Button >Enviar Email</Button>
+          }
+          <Error error={error}/>
+        </form>
+        )
+      }
     </section>
   )
 };
