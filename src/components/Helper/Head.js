7754341d0b1b8159c2597d80d3221a19138276e4
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-24 22:09:08 -0300

    feat: Head config

---
+++ src/components/Helper/Head.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const Head = (props) => {
+
+  useEffect(() => {
+    document.title = props.title + "| Little Paws";
+
+    document.querySelector("meta[name='description']")
+    .setAttribute('content', props.description || "");
+  }, [props]);
+
+  return <></>;
+};
+
+export default Head;
