ba5560f025bbdda71fe51bfad927452f219d4142
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-24 21:45:58 -0300

    feat: using request

--- src/components/LoginRecover/LoginRecover.js
+++ src/components/LoginRecover/LoginRecover.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { PASSWORD_LOST_FETCH } from '../../api';
+import useFetch from '../../hooks/useFetch';
 import useForm from '../../hooks/useForm';
 import Button from '../Form/Button/Button';
 import Input from '../Form/Input/Input';
+import Error from '../Helper/Error';
 
 const LoginRecover = () => {
-  const email = useForm('email');
-  function handleSubmit(event) {
+  const login = useForm();
+  const {data, loading, error, request} = useFetch();
+
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    if (login.validate()) {
+      const {url, options} = PASSWORD_LOST_FETCH({login: login.value, url: window.location.href.replace("pedeu", "resetar")});
+      const { json } = await request(url, options);
+    }
+
   }
 
   return (
@@ -17,9 +28,13 @@ const LoginRecover = () => {
           label="Email/Usuário"
           type="text"
           name="email"
-          {...email}
+          {...login}
         />
-        <Button >Enviar Email</Button>
+        {loading 
+          ? <Button disabled>Enviando...</Button>
+          : <Button >Enviar Email</Button>
+        }
+        <Error error={error}/>
       </form>
     </section>
   )
