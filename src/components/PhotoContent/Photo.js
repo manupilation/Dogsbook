42735b17bf2e2832e7cfaef393e67eba81f21762
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-24 21:23:43 -0300

    feat: Photo component

---
+++ src/components/PhotoContent/Photo.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { ONLY_PHOTO_GET_FETCH } from '../../api';
+import useFetch from '../../hooks/useFetch';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from './PhotoContent';
+
+const Photo = () => {
+  const {id} = useParams();
+  const {data, loading, error, request} = useFetch();
+
+  useEffect(() => {
+    async function fetchPhoto() {
+      const {url, options} = ONLY_PHOTO_GET_FETCH(id);
+      await request(url, options);
+    }
+    fetchPhoto();
+  }, [request, id]);
+
+  if (error) return <Error error={error}/>
+  if (loading) return <Loading/>
+  if(data)
+  return (
+    <section className='container mainContainer'>
+      <PhotoContent data={data} single={true}/>
+    </section>
+  )
+  else return null;
+}
+
+export default Photo
